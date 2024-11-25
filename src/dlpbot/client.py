88f145bdb0a339d853f5fdd723e0434bccbefe19
Simon Sawicki <contact@grub4k.xyz>
2024-11-26 00:51:10 +0100

    add edit support

--- src/dlpbot/client.py
+++ src/dlpbot/client.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import collections
 import logging
 
 import discord
@@ -15,6 +16,7 @@ class Client(discord.Client):
         intents.message_content = True
 
         self.__channel_ids: list[int] = channel_ids or []
+        self.__bad_messages: collections.deque[int] = collections.deque(maxlen=100)
         super().__init__(intents=intents)
 
     async def on_ready(self, /):
@@ -28,14 +30,28 @@ class Client(discord.Client):
         if message.channel.id not in self.__channel_ids:
             pass
 
+        await self.__handle_message(message)
+
+    async def on_message_edit(self, before: discord.Message, after: discord.Message, /):
+        if before.id not in self.__bad_messages:
+            return
+
+        logger.info(f"edited message by {after.author!r}]")
+        await self.__handle_message(after)
+
+    async def __handle_message(self, message: discord.Message, /):
         classification = await dlpbot.classifier.classify_message(message)
+        if message.id in self.__bad_messages:
+            self.__bad_messages.remove(message.id)
+
         if classification is dlpbot.classifier.Classification.NoLog:
             return
 
-        logger.info(f"{classification.name} sent by {message.author}")
+        logger.info(f"{classification.name} sent by {message.author!r}")
         if classification.has_verbose():
             return
 
+        self.__bad_messages.append(message.id)
         if classification.has_non_verbose():
             await message.reply(
                 "found non verbose log: please use `-v`",
@@ -67,8 +83,3 @@ class Client(discord.Client):
                 silent=True,
             )
             return
-
-    async def on_message_edit(self, before: discord.Message, after: discord.Message, /):
-        # TODO(Grub4K): when editing the message, rejudge based on cached if they were found before?
-        msg = f"{before.author} edited their message:\n{before.content} -> {after.content}"
-        logger.info(msg)
